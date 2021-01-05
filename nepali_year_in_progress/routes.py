dcd0107086f8c676f264b7460591166ba7cb87cb
Ayush Subedi <ayush.subedi@gmail.com>
2021-01-05 13:18:39 +0545

    resolve #7

--- nepali_year_in_progress/routes.py
+++ nepali_year_in_progress/routes.py
@@ -1,5 +1,13 @@
 from nepali_year_in_progress import application
+from nepali_year_in_progress import basic_auth
+
 
 @application.route('/')
 def hello():
     return 'hello'
+
+
+@application.route('/basicauth')
+@basic_auth.required
+def basicauthtest():
+    return 'it works'
